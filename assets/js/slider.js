b97a945570bd719fe2021f2878a9dc2903a89edb
Tran Thanh Ngoc <ngocyt001@gmail.com>
2022-07-23 09:36:24 +0700

    init project

---
+++ assets/js/slider.js
@@ -0,0 +1,64 @@
+
+// Slide
+
+
+var swiper = new Swiper(".food-slider", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    spaceBetween: 20,
+    pagination: {
+       el: ".swiper-pagination",
+       clickable: true,
+       color: "#666"
+    },
+    breakpoints: {
+       0: {
+         slidesPerView: 1,
+       },
+       700: {
+         slidesPerView: 2,
+       },
+       1000: {
+         slidesPerView: 3,
+       },
+    },
+ });
+ 
+ let previewContainer = document.querySelector('.food-preview-container');
+ let previewBox = previewContainer.querySelectorAll('.food-preview');
+ 
+ console.log(document.querySelectorAll('.food .slide'));
+ 
+ 
+ document.querySelectorAll('.food .slide').forEach(food =>{
+    food.onclick = () =>{
+       previewContainer.style.display = 'flex';
+       let name = food.getAttribute('data-name');
+       previewBox.forEach(preview =>{
+          let target = preview.getAttribute('data-target');
+          if(name == target){
+             preview.classList.add('active');
+          }
+       });
+    };
+ });
+ 
+ previewContainer.querySelector('#close-preview').onclick = () =>{
+    previewContainer.style.display = 'none';
+    previewBox.forEach(close =>{
+       close.classList.remove('active');
+    });
+ };
+ 
+ var swiper = new Swiper(".menu-slider", {
+    grabCursor:true,
+    loop:true,
+    autoHeight:true,
+    centeredSlides:true,
+    spaceBetween: 20,
+    pagination: {
+       el: ".swiper-pagination",
+       clickable: true,
+    },
+ });
